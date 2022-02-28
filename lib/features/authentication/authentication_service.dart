603e3324076a0c6ffbcf806c5c44e5b4623a4afb
luongpd <luongbeo1332000@gmail.com>
2022-02-28 12:55:26 +0700

    Update authentication_service.dart

--- lib/features/authentication/authentication_service.dart
+++ lib/features/authentication/authentication_service.dart
@@ -25,9 +25,9 @@ class AuthenticationServiceImpl extends AuthenticationService {
   }
 
   @override
-  Future<User> signInWithUsernameAndPassword(String username, String password) async {
+  Future<User> signInWithUsernameAndPassword(
+      String username, String password) async {
     await Future.delayed(Duration(seconds: 2));
-
     password = md5.convert(utf8.encode(password)).toString();
 
     var map = new Map<String, dynamic>();
@@ -35,8 +35,7 @@ class AuthenticationServiceImpl extends AuthenticationService {
     map['password'] = password;
     map['grant_type'] = 'password';
 
-    final response = await http.post(Uri.parse(
-        authApiUrl+'/oauth/token'),
+    final response = await http.post(Uri.parse(authApiUrl + '/oauth/token'),
         headers: {
           'Authorization': 'Basic YTJtOmJldGh1bmFtMjAyMA==',
           'Content-type': 'application/x-www-form-urlencoded',
@@ -45,15 +44,18 @@ class AuthenticationServiceImpl extends AuthenticationService {
 
     print(response.statusCode);
     print(response.body);
-    if(response.statusCode == 200){
+    if (response.statusCode == 200) {
       print("đăng nhập thành công");
-
       var info = jsonDecode(response.body);
 
+      var t = DateTime.now().add(Duration(seconds: info['expires_in']*1000));
+
+      print(t);
+
       saveInforUser(info['access_token'], info['userUid'], info['username']);
-    }else if(response.statusCode == 400){
+    } else if (response.statusCode == 400) {
       throw AuthenticationException(message: 'Wrong username or password');
-    }else {
+    } else {
       throw AuthenticationException(message: 'Error');
     }
 
@@ -63,16 +65,77 @@ class AuthenticationServiceImpl extends AuthenticationService {
   @override
   Future<void> signOut() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String? accessToken = prefs.getString('access_token');
+
+    if (accessToken != null) {
+      saveLogoutInfo();
+      final response = await http.post(
+          Uri.parse(authApiUrl +
+              '/api/logout/logout.do?access_token=' +
+              accessToken),
+          headers: {
+            'Authorization': 'Bearer ' + accessToken,
+            'Content-type': 'application/json',
+          });
+
+      if (response.statusCode == 200) {
+      } else {
+        throw Exception('Error');
+      }
+    }
+
     await prefs.remove("access_token");
     await prefs.remove("userUid");
     await prefs.remove("username");
   }
 
   saveInforUser(String token, String userUid, String username) async {
+    final response = await http.post(
+        Uri.parse(imwareApiHost + '/api/userInfo/saveLoginInfo'),
+        headers: {
+          'Authorization': 'Bearer ' + token,
+          'Content-type': 'application/json',
+        });
+
+    if (response.statusCode == 200) {
+    } else {
+      throw Exception('Error');
+    }
+
     SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.setString("access_token", token);
     await prefs.setString("userUid", userUid);
     await prefs.setString("username", username);
+  }
+
+  saveLogoutInfo() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String? accessToken = prefs.getString('access_token');
+
+    if (accessToken != null) {
+      final response = await http.post(
+          Uri.parse(imwareApiHost + '/api/userInfo/saveLogoutInfo'),
+          headers: {
+            'Authorization': 'Bearer ' + accessToken,
+            'Content-type': 'application/json',
+          });
+
+      if (response.statusCode == 200) {
+      } else {
+        throw Exception('Error');
+      }
+    } else {
+      final response = await http.post(
+          Uri.parse(imwareApiHost + '/api/userInfo/saveLogoutInfo'),
+          headers: {
+            'Content-type': 'application/json',
+          });
+
+      if (response.statusCode == 200) {
+      } else {
+        throw Exception('Error');
+      }
+    }
   }
 }
 
